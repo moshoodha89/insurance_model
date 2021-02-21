8926f49dd1e44daccafcb0cf9a972a513123fa28
moshoodha89 <moshoodha@gmail.com>
2021-02-21 23:07:49 +0000

    first committ here

---
+++ insurance.py
@@ -0,0 +1,57 @@
+# create the initial variables below
+age = 28
+sex = 0
+bmi = 26.2
+num_of_children = 3
+smoker = 0 
+
+# Add insurance estimate formula below
+insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+print("This person's insurance cost is ",  str(insurance_cost),  " dollars")
+
+# Age Factor
+age +=4
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+print ("\n")
+
+print("New insurance is ", str(new_insurance_cost))
+
+print ("\n")
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in cost of insurance after increasing the age by 4 years is " + str(change_in_insurance_cost) + " dollar")
+
+print ("\n")
+
+# BMI Factor
+age -=4
+print(age)
+bmi +=3.1
+print(bmi)
+
+print("\n")
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in estimated insurance cost after increasing BMI by 3.1 is " , str(change_in_insurance_cost) , "dollars.")
+
+
+# Male vs. Female Factor)
+bmi-=3.1
+
+sex = 1
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("\n")
+
+print("The change in estimated cost for being a male instead of female is " , str(change_in_insurance_cost) , "dollars.")
+# Extra Practice
+
